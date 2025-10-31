14048c474d9814304532ebf304919135882ef23d
sagarjain03 <141217003+sagarjain03@users.noreply.github.com>
2025-10-31 21:58:44 +0530

    conflicts resolved and bugs fixed

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
+import  asyncHandler  from "../utils/asyncHandler.js";
+import ApiError  from  "../utils/ApiError.js";
+import ApiResponse  from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 import { cryptoService } from "../utils/crypto.service.js";
 

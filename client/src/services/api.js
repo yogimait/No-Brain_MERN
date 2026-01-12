228c71323d1f97732727c7d0ea258d7ef219d156
SNEHA JHA <snehajha1230@gmail.com>
2026-01-12 18:05:19 +0530

    Change API base URL to production server

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api';
+//const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://no-brain-server.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
